31e331df5c55e1d33e90d8500ff466dff8780cb1
Jean Carlos <jean.silva0096@gmail.com>
2023-10-31 08:42:52 -0300

    Project created
    Co-authored-by: Leila Fernanda da Silva <leilafernandadasilva@gmail.com>

---
+++ src/main/java/br/com/jlstudents/CrudStudentsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jlstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudStudentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudStudentsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jlstudents/CrudStudentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jlstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudStudentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
